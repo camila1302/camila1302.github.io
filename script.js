ca26f1f4bc3c0791bdb33cb25ae8969d31f570b0
diascamila513 <diascamila513@gmail.com>
2018-08-30 08:40:43 -0400

    criando arquivos css e js

---
+++ script.js
@@ -0,0 +1,8 @@
+var txt1 =  document.getElementById('txt1')
+var button =  document.getElementById('button')
+var txt2 =  document.getElementById('txt2')
+
+button.addEventListener('click'). () => {
+  var txtcod = txt1.value.replace('/a/g')
+  
+})
